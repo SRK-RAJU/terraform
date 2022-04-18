398d6e2c3df417ba4e5d8c2e134014b31fdbd91c
RAJUKUMAR S <97968060+SRK-RAJU@users.noreply.github.com>
2022-04-18 19:56:19 +0530

    updating

--- examples/main.tf
+++ examples/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "terraform-b60"
+    bucket = "terra-raju"
     key    = "example/sample/terraform.tfstate"
     region = "us-east-1"
   }
