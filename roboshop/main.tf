3cef9f0389e8568645634db64bd68d418a299786
RAJUKUMAR S <97968060+SRK-RAJU@users.noreply.github.com>
2022-04-19 10:28:26 +0530

    updating

--- roboshop/main.tf
+++ roboshop/main.tf
@@ -32,14 +32,14 @@ resource "null_resource" "ansible" {
   provisioner "remote-exec" {
     connection {
       host     = element(aws_spot_instance_request.cheap_worker.*.private_ip, count.index)
-      user     = "root"
+      user     = "centos"
       password = "DevOps321"
     }
     inline = [
       "sudo yum install python3-pip -y",
       "sudo pip3 install pip --upgrade",
       "sudo pip3 install ansible",
-      "ansible-pull -U https://SUDDALARAJUKUMAR@dev.azure.com/SUDDALARAJUKUMAR/ROBOSHOP/_git/ansible roboshop-pull.yml -e ansible_user=SUDDALARAJUKUMAR ansible_password=frb3woctre3lpfjyuhxjsz432ogx5nbuvzokwhzagmcdkaml7w5q  -e COMPONENT=${element(var.components, count.index)} -e ENV=dev"
+      "ansible-pull -U https://SUDDALARAJUKUMAR@dev.azure.com/SUDDALARAJUKUMAR/ROBOSHOP/_git/ansible roboshop-pull.yml -e ansible_user=root ansible_password=frb3woctre3lpfjyuhxjsz432ogx5nbuvzokwhzagmcdkaml7w5q  -e COMPONENT=${element(var.components, count.index)} -e ENV=dev"
     ]
   }
 }
@@ -58,6 +58,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-#locals {
-#  COMP_NAME = element(var.components, count.index)
-#}
+locals {
+COMP_NAME = element(var.components, count.index)
+}
